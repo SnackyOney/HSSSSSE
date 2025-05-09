b5364dd02b24ce243d471d68a83a0c66d3344bd4
PedreAndreev <uvarov.aa@phystech.edu>
2025-05-09 18:55:00 +0300

    ld

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import { getCanvas, getScoreText } from "./game.js";
 
-export const doc = document;
+export const getDocument = () => {
+  return document;
+};
 export const body = document.querySelector("body");
 export const gosTemplate = document.querySelector(
   ".game-over-screen-template"
