8d279a8f028c39d5c75ce007e42f2da37c8da467
Артём <144065809+SnackyOney@users.noreply.github.com>
2025-05-11 18:03:07 +0300

    Update gameObject.js

--- gameObject.js
+++ gameObject.js
@@ -5,7 +5,7 @@ export class GameObject {
   constructor({ position }) {
     this.position = position ?? new Vector2(0, 0);
     this.children = [];
-    this.parent = null;
+    this.parent = null; 
   }
 
   stepEntry(delta, root) {
