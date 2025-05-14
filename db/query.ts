3c073604a16c772bcb59ff7de468ca1d28873783
PedreAndreev <uvarov.aa@phystech.edu>
2025-05-14 22:11:52 +0300

    fin

--- db/query.ts
+++ db/query.ts
@@ -2,19 +2,19 @@ import { kv } from "./kv.ts";
 
 export async function get_records() {
   const entries = kv.list({ prefix: ["results"] });
-  const results = [];
+  let results = [];
 
   console.log("start get");
   for (let i = 0; i < Math.min(100000, entries.length); ++i) {
     results.push(entries[i].value);
-    // console.log(entries[i].value);
+    console.log(entries[i].value);
   }
 
   // for await (const entry of entries) {
   //   results.push(entry.value);
   // }
 
-  console.log("finish get");
+  console.log("finish get", results.length);
 
   return results;
 }
